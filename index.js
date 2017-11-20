3db6545ca4aba157e06730ccb04e2613cce6af34
Mariel Schlaefer <mschlaefer00@gmail.com>
2017-11-20 18:56:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,15 +8,15 @@ function happyHolidays() {
 }
 
 function happyHolidaysTo(name) {
-  return ("Happy holidays, ${name}!")
+  return "Happy holidays, ${name}!"
 }
 
 function happyHolidayTo(holiday, name) {
-  return ("Happy ${holiday}, ${name}!")
+  return "Happy ${holiday}, ${name}!"
 }
 
 holiday = "Mother's Day"
 
 function holidayCountdown(days, holiday) {
-  return ("It's ${days} days until ${holiday}!")
+  return "It's ${days} days until ${holiday}!"
 }
